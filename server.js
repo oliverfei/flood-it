7c71b017d38eb2155e30da530c488b5bf0fa3bab
Oliver Fei <ofei006@uottawa.ca>
2017-12-01 22:13:22 -0500

    Initialize node.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
+app.get('/', (req, res) => res.render("index.html"));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
